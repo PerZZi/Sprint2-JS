1b6c00de0f64a83303145b5dabdf94df5cf40a22
PerZZi <emiliano.estebandiaz@gmail.com>
2023-11-14 13:16:43 -0300

    version2

--- main.js
+++ main.js
@@ -1,55 +1,64 @@
-const conteCards=document.getElementById("cartas")
+const conteCards = document.getElementById("cartas")
 
-function crearCards(objeto){
-    let card= `<article class="h-[450px] min-w-[300px] w-1/5 border-2 border-black rounded-lg bg-white flex flex-col gap-5 px-5 pb-5">
+function crearCards(objeto) {
+    return `<article class="h-[450px] min-w-[300px] w-1/5 border-2 border-black rounded-lg bg-white flex flex-col gap-5 px-5 pb-5">
     <img class="pt-3 h-[200px]" src="${objeto.image}" alt="">
     <h2 class="text-black text-center">${objeto.title}</h2>
-    <p class="text-black text-center">${objeto.overview.slice(0,150)}...</p>
+    <p class="text-black text-center line-clamp-3">${objeto.overview}...</p>
     <a class="flex justify-center p-1 rounded-lg bg-violet-800 text-white" href="favs.html?id=${objeto.id}">Detalles</a>
 </article>`
-    return card
 }
-console.log(crearCards(movies[0]))
+console.log([crearCards(movies)])
 
-for (let indice = 0; indice < movies.length; indice++) {
-    conteCards.innerHTML = conteCards.innerHTML + crearCards(movies[indice])
-
+function imprimirTemplate(lista, contenedor, fn) {
+    let template = ""
+    for (const generoIterado of lista) {
+        template += fn(generoIterado)
+    }
+    contenedor.innerHTML = template
 }
 
-
+ console.log([imprimirTemplate(movies,conteCards,crearCards)])
 //sprint 2
 
-// const generos = movies.map(genero => genero.genres)
-// console.log(...generos)
-
-const genres = new Set()
-for (const movie of movies) {
-    genres.add(...movie.genres)
-}
-console.log(genres)
-
+const generos = movies.map(genero => genero.genres).flat()
+const listaGeneros = new Set(generos)
+console.log(listaGeneros)
 
 const selec = document.getElementById("elegir")
 const textoPeli = document.getElementById("textoGenero")
 
 
-textoPeli.addEventListener("input", () => {
-    const inputText = textoPeli.value.toLowerCase();
-    const filteredMovies = movies.filter(movie => {
-       
-        return movie.genres.some(genre => genre.toLowerCase().includes(inputText));
-    });
-    selec.innerHTML = '';
-    filteredMovies.forEach(movie => {
-        const option = document.createElement('option');
-        option.text = movie.title;
-        selec.add(option);
-    });
+textoPeli.addEventListener("keyup", () => {
+    const filtroTitulo = filtrarPorNombre(movies, textoPeli.value)
+    imprimirTemplate(filtroTitulo,conteCards,crearCards)
+    console.log()
 });
 
-function filtrarPorNombre(movies, nombre) {
-    const filtrarNombre = movies.filter(movie => movie.title.toLowerCase().startsWith(nombre.toLowerCase()))
-    return filtrarNombre
+
+function filtrarPorNombre(listado, titulos) {
+    const filtro = listado.filter(movie => movie.title.toLowerCase().startsWith(titulos.toLowerCase()))
+    return filtro
 }
+console.log(filtrarPorNombre(movies,"the nun"))
+
+function createSelector(generosFiltrados) {
+    return `<option value=${generosFiltrados}>${generosFiltrados}</option>`
+}
+
+function filtrarPorGenero(listaPeliculas,genero){
+    const filtroDeGenero = listaPeliculas.filter(movie => movie.genres.includes(genero))
+    return filtroDeGenero
+}
+
+selec.addEventListener("input", () =>{
+    const opcion = selec.value
+    const filtroGenero = filtrarPorGenero(movies,opcion)
+    imprimirTemplate(filtroGenero,conteCards,createSelector)
+    
+})
+imprimirTemplate(listaGeneros,selec,createSelector)
+
+console.log(listaGeneros)
 
 
